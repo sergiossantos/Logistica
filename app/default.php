e1d110d9af4094826d1e1c1eddaeb40e168afc23
Guilherme Toti <guilhermetoti@gmail.com>
2013-10-24 10:16:29 -0200

    Adicionado os arquivos principais

---
+++ app/default.php
@@ -0,0 +1,49 @@
+<?php 
+
+# Está online?
+define('ONLINE', false);
+
+# Timezone
+define('DEFAULT_TIMEZONE', 'America/Sao_Paulo');
+
+# Set Timezone
+date_default_timezone_set(DEFAULT_TIMEZONE);
+
+# Dados de conexão
+if(ONLINE)
+{
+    # Dados para conexão online
+    $server = "";
+    $user = "";
+    $pass = "";
+    $bd = "";
+
+    # Base URL
+    define('BASE_URL', '');
+    define('PATH_IMG', '');
+}
+else
+{    
+    # Dados para conexão local
+    $server = "localhost";
+    $user = "root";
+    $pass = "";
+    $bd = "";
+
+    # Base URL
+    define('BASE_URL', 'http://localhost/padrao/');
+}
+
+define('BASE_PATH', dirname(__FILE__) . '/../');
+
+try
+{
+    $pdo = new PDO("mysql:host=$server;dbname=$bd", $user, $pass);
+}
+catch(PDOException $e)
+{
+    echo '<pre>Falha na conexão com o banco de dados. <br/>Dados do erro: ' . $e->getMessage() . '</pre>';
+}
+
+# Pages
+$page = isset($_GET['page']) ? mysql_real_escape_string($_GET['page']) : "";
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php 
+include_once('app/default.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="author" content="GT DESIGN | www.gtsites.com.br">
+  <meta name="description" content="">
+  <title>Padrão de Projeto</title>
+  <!-- Bootstrap -->
+  <link href="<?php echo BASE_URL; ?>files/css/bootstrap.min.css" rel="stylesheet" media="screen">
+  <!-- <link rel="shortcut icon" href="../../assets/ico/favicon.png"> -->
+
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="<?php echo BASE_URL; ?>files/js/html5shiv.js"></script>
+      <script src="<?php echo BASE_URL; ?>files/js/respond.min.js"></script>
+      <![endif]-->
+    </head>
+    <body>
+     <!-- Start codin' :) -->
+     <div class="container">
+      
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="<?php echo BASE_URL; ?>files/js/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php echo BASE_URL; ?>files/js/bootstrap.min.js"></script>
+  </body>
+  </html>
